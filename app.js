8102a4280fbccb3d55eba44268a54f44e27dfd1a
ChaseTerzian <chaseterzian@gmail.com>
2015-06-03 00:42:44 -0600

    initial commit: 4 different test plot structures, swing, spin1, spin2, spin3, runIndoor

---
+++ app.js
@@ -0,0 +1,23 @@
+require('./data.js');
+
+function highestG(data) {//consoleZ max
+	var highG = 0;
+	for (var i=0; i<data.length; i++) {
+		if (highG < data[i][3]) { highG = data[i][3]; }
+	}
+	return highG;
+}
+
+function allAxes(data) {//consoleXYZ with time
+	var dataXYZ = [['X',0,0],['Y',0,0],['Z',0,0]];
+	for (var i=0; i<data.length; i++) {
+		if (dataXYZ[0][2] < data[i][1]) { dataXYZ[0][2] = data[i][1]; dataXYZ[0][1] = data[i][0]; }
+		if (dataXYZ[1][2] < data[i][2]) { dataXYZ[1][2] = data[i][2]; dataXYZ[1][1] = data[i][0]; }
+		if (dataXYZ[2][2] < data[i][3]) { dataXYZ[2][2] = data[i][3]; dataXYZ[2][1] = data[i][0]; }
+	}
+	return dataXYZ;
+}
+// console.log(allAxes(sData));
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+//
